74dc45062f43f9b4abfbca738ffa6b56db4d5daa
Aleksei <Edelbert@users.noreply.github.com>
2026-02-10 20:05:24 +0300

    module02-lesson-06 (#10)
    * introduce bugs and project file
    
    * roll back

--- lesson-06-pytests/preprocessing.py
+++ lesson-06-pytests/preprocessing.py
@@ -2,17 +2,23 @@
 import pandas as pd
 import numpy as np
 
+
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """Выполняет предобработку данных о поездках Uber."""
     # 1. Фильтрация по стоимости и пассажирам
-    df_filtered = df[(df['fare_amount'] > 0) & 
-                     (df['passenger_count'] > 0) & 
-                     (df['passenger_count'] <= 6)]
+    mask = (
+        (df["fare_amount"] > 0)
+        & (df["passenger_count"] > 0)
+        & (df["passenger_count"] <= 6)
+    )
+
+    df.drop(index=df.loc[~mask].index, inplace=True)
 
     # 2. Создание нового признака 'distance'
-    df_filtered['distance'] = np.sqrt(
-        (df_filtered['dropoff_longitude'] - df_filtered['pickup_longitude'])**2 +
-        (df_filtered['dropoff_latitude'] - df_filtered['pickup_latitude'])**2
+    df["distance"] = np.sqrt(
+        (df["dropoff_longitude"] - df["pickup_longitude"]) ** 2
+        + (df["dropoff_latitude"] - df["pickup_latitude"]) ** 2
     )
-    final_features = df_filtered[['distance', 'passenger_count']]
+    final_features = df[["distance", "passenger_count"]]
     return final_features
+
